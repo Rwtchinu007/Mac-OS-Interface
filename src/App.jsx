b23f831459fe8a3132bb48036c0a1eb12379f904
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-03-17 22:57:21 +0530

    Dock Ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './app.scss'
+import Dock from './components/Dock'
+// Install sass before using scss files
+const App = () => {
+  return (
+    <main>
+      <Dock/>
+    </main>
+  )
+}
+
+export default App
